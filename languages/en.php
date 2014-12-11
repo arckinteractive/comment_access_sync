57db1bc0a82e6e705e43fb70dc1c17695b1ff08c
Matt Beckett <beck24@gmail.com>
2014-12-10 20:49:01 -0800

    remove unnecessary files

--- languages/en.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$english = array(
-	'comment_access_sync:admin:notice' => "Comment/Discussion access sync issues have been detected.  Click here for %s",
-	'comment_access_sync:findoutmore' => "More Information",
-);
-
-add_translation('en', $english);
